8ab90eaaf75b95d7f44c4e5f133e056c60b06504
Hosna Qasmei <hosna.qasmei@gmail.com>
2023-05-23 23:05:12 -0700

    Update header from fixed to sticky

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,8 +1,18 @@
-import React from 'react'
+"use client";
+import React from "react";
+
+import { useScrollPosition } from "@/hooks/useScrollPosition";
 
 const Header = () => {
+  const scrollPosition = useScrollPosition();
   return (
-    <header className="text-gray-600 body-font">
+    <header
+      className={`sticky top-0 z-50 transition-shadow ${
+        scrollPosition > 0
+          ? "shadow bg-opacity-70 backdrop-blur-lg backdrop-filter"
+          : "shadow-none"
+      }`}
+    >
       <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
         <a className="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0">
           <svg
@@ -42,6 +52,6 @@ const Header = () => {
       </div>
     </header>
   );
-}
+};
 
-export default Header
+export default Header;
---
+++ src/hooks/useScrollPosition.tsx
@@ -0,0 +1,18 @@
+"use client";
+import React, { useState, useEffect } from "react";
+
+export const useScrollPosition = () => {
+  const [scrollPosition, setScrollPosition] = useState(0);
+
+  useEffect(() => {
+    const updatePosition = () => {
+      setScrollPosition(window.pageYOffset);
+    };
+
+    window.addEventListener("scroll", updatePosition);
+    updatePosition();
+    return () => window.removeEventListener("scroll", updatePosition);
+  }, []);
+
+  return scrollPosition;
+};
